07723cf00c880c65e58a7ff0110b31b4a7fc2a72
fearhunt <firhanmahdi@gmail.com>
2021-07-30 22:55:46 +0700

    init commit

---
+++ components/navigations/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <footer id="footer" class="bg-primary">
+    <b-container>
+      <b-row>
+        <b-col sm="12" md="2" class="my-auto">
+          <h2 class="brand-title">Gobi</h2>
+        </b-col>
+        <b-col sm="12" md="10">
+          <b-row>
+            <b-col sm="12" md="3" class="text-md-center">
+              <h4 class="nav-title">About</h4>
+              <ul class="footer-nav">
+                <b-nav-item v-for="(link, index) in about" :key="index" :to="link.url">
+                  {{ link.title }}
+                </b-nav-item>
+              </ul>
+            </b-col>
+            <b-col sm="12" md="3" class="text-md-center">
+              <h4 class="nav-title">Support</h4>
+              <ul class="footer-nav">
+                <b-nav-item v-for="(link, index) in support" :key="index" :href="link.url">
+                  {{ link.title }}
+                </b-nav-item>
+              </ul>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        about: [
+          { title: "Blog", url: "/?page=here" },
+          { title: "Contact Us", url: "/?page=here" },
+        ],
+        support: [
+          { title: "Help", url: "/?page=here" },
+          { title: "Privacy Policy", url: "/?page=here" },
+          { title: "Terms & Conditions", url: "/?page=here" },
+        ]
+      }
+    },
+
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ store/alert.js
@@ -0,0 +1,18 @@
+import swal from 'sweetalert';
+
+export const actions = {
+  displaySuccess(context, res) {
+    console.log(res);
+    swal("Notification", res.message, "success");
+  },
+
+  displayError(context, err) {
+    console.log(err);
+    swal("Notification", err.message, "error");
+  },
+
+  displayMessage(context, res) {
+    console.log(res);
+    swal("Notification", res.message);
+  }
+}
